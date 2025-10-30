9df56f282f28151445c1eba159c2c8c606f26cb3
Vignesh1116 <vigneshbalan1116@gmail.com>
2025-10-30 20:12:02 +0530

    Add initial Flask application with home and user routes

--- sample.py
+++ sample.py
@@ -1,14 +1,18 @@
-from flask import Flask
+# from flask import Flask,redirect,url_for
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello! This is the main page <h1>HELLO</h1>"
+# @app.route("/")
+# def home():
+#     return "Hello! this is the msin page <h1>HELLO</h1>"
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello, {name}!"
+# @app.route("/<namw>")
+# def user(namw):
+#     return f"Hello {name}!"
 
-if __name__ == "__main__":
-    app.run()
+# @app.route("/admin")
+# def admin():
+#     return redirect(url_for("home"))
+
+# if __name__ == "__main__":
+#     app.run()
