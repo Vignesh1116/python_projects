80c7b5682432db7d7ecfc6a1542b94979ed0931a
Vignesh B <vigneshbalan1116@gmail.com>
2025-11-03 10:05:58 +0530

    Uncomment Flask app routes and main execution

--- sample.py
+++ sample.py
@@ -1,18 +1,18 @@
-# from flask import Flask,redirect,url_for
+from flask import Flask,redirect,url_for
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
-# def home():
-#     return "Hello! this is the msin page <h1>HELLO</h1>"
+@app.route("/")
+def home():
+    return "Hello! this is the msin page <h1>HELLO</h1>"
 
-# @app.route("/<name>")
-# def user(name):
-#     return f"Hello {name}!"
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
 
-# @app.route("/admin")
-# def admin():
-#     return redirect(url_for("home"))
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 
-# if __name__ == "__main__":
-#     app.run() 
+if __name__ == "__main__":
+    app.run() 
